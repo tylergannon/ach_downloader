df5111e2d0e1e61df0670291ab5f7b0c36538b8e
Tyler Gannon <tgannon@gmail.com>
2011-06-16 07:53:34 -0700

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AchDownloader::Application.config.secret_token = '1cdff5dd29bfb6f61b669efd616b8af0b439bcbc52e77552456c5cfaed2d82e65f901cf2b49b0c78bffb182a91bfe27829db1f1ad1e0e37cd6a527347ca36758'
